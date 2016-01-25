849a13332dd6fe5f9c5a552ff4697ef03af87a1d
Jae Kwon <jae@tendermint.com>
2016-01-24 18:50:01 -0800

    First commit

---
+++ bytearray.js
@@ -0,0 +1,22 @@
+var buffer = require('buffer');
+var Reader = require('./reader').Reader;
+var Writer = require('./writer').Writer;
+
+Reader.prototype.readByteArray = function() {
+  var length = this.readUvarint();
+  var buf = new Uint8Array(length);
+  for (var i = 0; i < length; i += 1) {
+    var b = this.readByte();
+    buf[i] = b;
+  }
+  return buf
+}
+
+Writer.prototype.writeByteArray = function(v) {
+  var length = v.length;
+  this.writeUvarint(length);
+  for (var i = 0; i < length; i++) {
+    var b = v[i];
+    this.writeByte(b);
+  }
+}
